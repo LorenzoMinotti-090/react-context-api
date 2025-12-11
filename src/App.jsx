b6f4665b2ca207157403a3064b5bc1dd6b816574
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2025-12-11 15:54:22 +0100

    docs: context-api

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import DefaultLayout from "./layouts/DefaultLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import SingleProduct from "./pages/SingleProduct";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chi-siamo" element={<About />} />
+          <Route path="/prodotti" element={<Products />} />
+          <Route path="/prodotti/:id" element={<SingleProduct />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
